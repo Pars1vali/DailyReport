947c1549d981b93f78c667b46e2eef3024e0b998
Chumak Vladimir <parsival427@gmail.com>
2025-04-13 14:08:14 +0300

    Изменено, теперь данные для составления формы отчета загружаются из json файла.

--- group.py
+++ group.py
@@ -1,75 +1,3 @@
-class GroupTopic:
-    def __init__(self, topics: list):
-        self.topics = topics
-
-
-class PlanFactValue:
-    def __init__(self, plan, fact):
-        self.plan = plan
-        self.fact = fact
-
-
-class CreditValue:
-    def __init__(self, loan_apply, approved, issued):
-        self.loan_apply = loan_apply
-        self.approved = approved
-        self.issued = issued
-
-
-class TopicValue:
-    def __init__(self, number=0, plan_fact: PlanFactValue = None, credit: CreditValue = None):
-        self.number = number
-        self.plan_fact = plan_fact
-        self.credit = credit
-
-
-class Topic:
-    def __init__(self, text: str, unit, is_credit: bool = False, have_plan: bool = False,
-                 value: TopicValue = TopicValue):
-        self.text = text
-        self.unit = unit
-        self.is_credit = is_credit
-        self.have_plan = have_plan
-        self.value = value
-
-
-group_topics = list([
-    GroupTopic([
-        Topic("Сим Мегафон+Yota, шт", 0),
-        Topic("Мегасемья", 0)
-    ]),
-    GroupTopic([
-        Topic("Абонементы мегафон +ета", 0, have_plan=True),
-        Topic("Доля абонементов от сим", 0.0)
-    ]),
-    GroupTopic([
-        Topic("РК REALME(модели по акции)", 0),
-        Topic("РК HONOR(модели по акции)", 0),
-        Topic("Первая цена", 0),
-        Topic("Хуавей", 0),
-        Topic("Apple", 0)
-    ]),
-    GroupTopic([
-        Topic("Аксесуары", 0.0),
-        Topic("Акция 1+1", 0.0)
-    ]),
-    GroupTopic([
-        Topic("ФУ", 0.0),
-        Topic("Плотер", 0),
-        Topic("Страховки", 0.0),
-    ]),
-    GroupTopic([
-        Topic("КД шт", 0, is_credit=True),
-        Topic("БК шт", 0, is_credit=True)
-    ]),
-    GroupTopic([
-        Topic("Адаптер", 0)
-    ]),
-    GroupTopic([
-        Topic("NPS 9/10 факт за день", 0)
-    ])
-])
-
 opio_list = list([
     "Став 189",
     "Став 141",
@@ -92,14 +20,17 @@ opio_list = list([
 
 
 def make_message_report(opio_name: str, group_topics):
-    message_report = f"🟢🟢🟣\nОфис - {opio_name}\n"
+    message_report = f"Офис = {opio_name}\n"
     for group in group_topics:
-        message_report += "---\n"
-        for topic in group.topics:
-            if topic.have_plan:
-                message_report += f"{topic.text} (план/факт) - {topic.value.plan_fact.plan}/{topic.value.plan_fact.fact}\n"
-            elif topic.is_credit:
-                message_report += f"{topic.text} (заявки/одобрено/выдано) - {topic.value.credit.loan_apply}/{topic.value.credit.approved}/{topic.value.credit.issued}\n"
+        message_report += "🟢\n"
+        for topic in group:
+            if topic["have_plan"] is True:
+                text, value = topic["text"], topic["value"]
+                message_report += f'{text} = [{value["plan"]},{value["fact"]}]\n'
+            elif topic["is_credit"] is True:
+                text, value = topic["text"], topic["value"]
+                message_report += f'{text} = [{value["loan_apply"]},{value["approved"]},{value["issued"]}]\n'
             else:
-                message_report += f"{topic.text} - {topic.value.number}\n"
+                message_report += f'{topic["text"]} = {topic["value"]}\n'
+
     return message_report
--- main.py
+++ main.py
@@ -1,84 +1,97 @@
+import json
 import streamlit as st
 import logging
-import bot
-import group
-from group import group_topics, opio_list, make_message_report
+import bot, group
+from group import opio_list, make_message_report
 
-form = dict()
+form = list()
 logging.getLogger().setLevel(logging.INFO)
 
-def credit_topic(topic: group.Topic, index_topic):
-    st.markdown(f"**{topic.text}**")
+
+def credit_topic(topic, index_topic):
+    text_topic = topic["text"]
+    st.markdown(f"**{text_topic}**")
 
     col1, col2, col3 = st.columns(3)
     with col1:
-        loan_apply = st.number_input("Заявки", value=topic.unit, min_value=topic.unit, key=f"{index_topic}loan_apply")
+        loan_apply = st.number_input("Заявки", value=topic["unit"], min_value=topic["unit"],
+                                     key=f"{index_topic}loan_apply")
     with col2:
-        approved = st.number_input("Одобрено", value=topic.unit, min_value=topic.unit, key=f"{index_topic}approved")
+        approved = st.number_input("Одобрено", value=topic["unit"], min_value=topic["unit"],
+                                   key=f"{index_topic}approved")
     with col3:
-        issued = st.number_input("Выдано", value=topic.unit, min_value=topic.unit, key=f"{index_topic}issued")
+        issued = st.number_input("Выдано", value=topic["unit"], min_value=topic["unit"],
+                                 key=f"{index_topic}issued")
 
-    topic.value.credit = group.CreditValue(loan_apply, approved, issued)
-
-    form[topic.text] = {
-        "Заявки": loan_apply,
-        "Одобрено": approved,
-        "Выдано": issued
+    return {
+        "text": text_topic,
+        "value": {
+            "loan_apply": loan_apply,
+            "approved": approved,
+            "issued": issued
+        },
+        "is_credit": True,
+        "have_plan": False
     }
 
-
-def plan_fact_topic(topic: group.Topic, index_group):
-    st.markdown(f"**{topic.text}**")
+def plan_fact_topic(topic, index_group):
+    text_topic = topic["text"]
+    st.markdown(f"**{text_topic}**")
 
     col1, col2 = st.columns(2)
     with col1:
-        plan = st.number_input("План", value=topic.unit, min_value=topic.unit, key=f"{index_group}plan")
+        plan = st.number_input("План", value=topic["unit"], min_value=topic["unit"], key=f"{index_group}plan")
     with col2:
-        fact = st.number_input("Факт", value=topic.unit, min_value=topic.unit, key=f"{index_group}fact")
+        fact = st.number_input("Факт", value=topic["unit"], min_value=topic["unit"], key=f"{index_group}fact")
 
-    topic.value.plan_fact = group.PlanFactValue(plan, fact)
+    return {
+        "text": text_topic,
+        "value": {
+            "plan": plan,
+            "fact": fact
+        },
+        "is_credit": False,
+        "have_plan": True
+    }
 
-    form[topic.text] = {
-        "План": plan,
-        "Факт": fact
+def number_topic(topic):
+    value_topic = st.number_input(topic["text"], value=topic["unit"],
+                                  min_value=topic["unit"])
+    return {
+        "text": topic["text"],
+        "value": value_topic,
+        "is_credit": False,
+        "have_plan": False
     }
 
 def main():
+    with open('report_daily.json', encoding='utf-8') as file:
+        data = json.load(file)
 
     with st.form("Отчет"):
+        st.header("Отчет")
         opio_name = st.selectbox("Название вашего ОПиО", opio_list, index=None, placeholder="ОПиО")
         photo_cheque = st.file_uploader("Отчет без гашения", type=["jpg", "jpeg", "png"])
 
-        form["Название ОПиО"] = opio_name
-
-        for index_group, group in enumerate(group_topics):
-
-            st.divider()
-            for index_topic, topic in enumerate(group.topics):
-
-                if topic.is_credit:
-                    credit_topic(topic, index_topic)
-                elif topic.have_plan:
-                    plan_fact_topic(topic, index_group)
+        for index_group, group in enumerate(data["topics"]):
+            group_unit = list()
+            form.append(group_unit)
+            for index_topic, topic in enumerate(group):
+                if topic["is_credit"] is True:
+                    group_unit.append(credit_topic(topic, index_topic))
+                elif topic["have_plan"] is True:
+                    group_unit.append(plan_fact_topic(topic, index_group))
                 else:
-                    topic.value.number = st.number_input(topic.text, value=topic.unit, min_value=topic.unit)
-                    form[topic.text] = topic.value.number
+                    group_unit.append(number_topic(topic))
+
 
         send = st.form_submit_button("Отправить", use_container_width=True)
 
         if send and opio_name is not None and photo_cheque is not None:
-            # st.write(form)
-
-            # chat_id = st.query_params["chat_id"]
-            # type_report = st.query_params["type_report"]
-
-            # st.write(chat_id, type_report)
-
-
-            message_report = make_message_report(opio_name, group_topics)
+            message_report = make_message_report(opio_name, form)
             bot.send_report(message_report, photo_cheque)
             st.success("Отчет отправлен!")
-            # st.balloons()
+            st.balloons()
 
 
 if __name__ == "__main__":
