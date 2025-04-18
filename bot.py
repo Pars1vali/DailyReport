b35b886966a5211cf31816cc150a777312ea9583
Chumak Vladimir <parsival427@gmail.com>
2025-04-18 16:38:05 +0300

    Добавлено: отметка для опио кто прислал отчет.

--- bot.py
+++ bot.py
@@ -18,8 +18,8 @@ def set_report_complete(opio_name, reply_message_id, chat_id,  char_status):
     opio, probability = process.extract(opio_name, group.opio_list, limit=1)[0]
     message_text = bot.copy_message(chat_id, chat_id, reply_message_id)
     report_message = re.sub(
-        f'{opio} - [{group.char_complete_opio}{group.char_time_status}{group.char_default_status}{group.char_stop_opio}]', \
-        f"{opio} - {char_status}", \
+        f'{opio} - [{group.char_complete_opio}{group.char_time_status}{group.char_default_status}{group.char_stop_opio}{group.char_none_report_status}]',
+        f"{opio} - {char_status}",
         message_text)
     bot.edit_message_text(chat_id=chat_id, message_id=reply_message_id,
                                 text=report_message)
