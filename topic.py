60bfc5af63f60e4412a8c36ed60fade477a5df2d
Chumak Vladimir <parsival427@gmail.com>
2025-04-24 08:35:26 +0300

    Изменен формат сообщений. В методы создания топиков добавлена запись единиц измерения.

--- topic.py
+++ topic.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def credit(topic, index):
-    text_topic = f"{topic["text"]} (заявки/одобрено/выдано)"
+    text_topic = f'{topic["text"]} (заявки/одобрено/выдано)'
     emoji = topic.get("emoji", "🟢")
     st.markdown(f"**{text_topic}**")
 
@@ -30,7 +30,7 @@ def credit(topic, index):
     }
 
 def plan_fact(topic, index):
-    text_topic = f"{topic["text"]} (план/факт)"
+    text_topic = f'{topic["text"]} (план/факт)'
     emoji = topic.get("emoji", "🟢")
     st.markdown(f"**{text_topic}**")
 
@@ -53,7 +53,7 @@ def plan_fact(topic, index):
     }
 
 def share(topic, index):
-    text_topic = f"{topic["text"]} %"
+    text_topic = f'{topic["text"]} %'
     emoji = topic.get("emoji", "🟢")
     st.markdown(f"**{text_topic}**")
 
@@ -79,7 +79,7 @@ def share(topic, index):
 
 def number(topic, index_group, index_topic):
     unit = "руб" if topic["type"] == "money" else "шт"
-    text_topic = f"{topic["text"]}, {unit}"
+    text_topic = f'{topic["text"]}, {unit}'
     value_topic = st.number_input( text_topic, value=topic["unit"],
                                   min_value=topic["unit"], key=f"{index_group}_{index_topic}_number")
     emoji = topic.get("emoji", "🟢")
