991c990cc51e73024d4bdd72db304931ec531abc
Chumak Vladimir <parsival427@gmail.com>
2025-04-24 08:37:27 +0300

    Изменен формат сообщений. В методы создания топиков добавлена запись единиц измерения.

--- report.py
+++ report.py
@@ -39,7 +39,7 @@ def create_message(opio_name: str, group_topics):
     logging.info("Create message format tg fro groupe sales.")
     message_report = f"Офис = {opio_name}\n"
     for group in group_topics:
-        message_report +="\n"
+        # message_report +="\n"
         for topic in group:
             message_report += topic["emoji"]
             if topic["have_plan"] is True:
