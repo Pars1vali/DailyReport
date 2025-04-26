8bb8fd69761ae5d7efc0b422df7782cfb6cbad6a
Chumak Vladimir <parsival427@gmail.com>
2025-04-26 06:56:31 +0300

    Для денежных topic шаг при нажатии на + сделал 1000

--- form_service.py
+++ form_service.py
@@ -114,6 +114,7 @@ class Form:
     def create_number_topic(topic: dict) -> dict:
         topic_type = topic.get("type", "number")
         unit_name = "руб." if topic_type == "money" else "шт."
+        unit_step = 1000 if topic_type == "money" else 1
         unit_value = topic.get("unit", 0)
         topic_text = f'{topic["text"]}, {unit_name}'
         emoji = topic.get("emoji", "🟢")
@@ -121,6 +122,7 @@ class Form:
         value_topic = st.number_input(topic_text,
                                       value=unit_value,
                                       min_value=unit_value,
+                                      step=unit_step,
                                       key=f"{id(topic)}_number")
 
         return {
